26caa007c3da35487523c55903dadca6a15c2b4a
John Doe1~ <abdelazizelrasheed@gmail.com>
2025-05-06 09:33:05 +0200

    last api

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: 'https://api.tmrrealestate.org/api/'
   // withCredentials: true,
 
 })
